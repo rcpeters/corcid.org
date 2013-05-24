ea5340dc72ae46d219de2b7a791e8b28156ff19e
rcpeters <info@rcpeters.com>
2013-05-24 12:52:03 -0700

    done, at leat for now

--- README.md
+++ README.md
@@ -1,4 +1,26 @@
-corcid.org
-==========
+#[CORCID.org](https://github.com/rcpeters/corcid.org)
+Horrible, horrible and yet delightful hack.
 
-Vanity URL login
+***
+
+####CORCID.org is built on top of the following libraries :
+
+* [Node.js](http://nodejs.org/) - Application Server
+* [Express.js](http://expressjs.com/) - Node.js Web Framework
+* [MongoDb](http://www.mongodb.org/) - Database Storage
+* [Jade](http://jade-lang.com/) - HTML Templating Engine
+* [Stylus](http://learnboost.github.com/stylus/) - CSS Preprocessor
+* [EmailJS](http://github.com/eleith/emailjs) - Node.js > SMTP Server Middleware
+* [Moment.js](http://momentjs.com/) - Lightweight Date Library
+* [Twitter Bootstrap](http://twitter.github.com/bootstrap/) - UI Component & Layout Library
+
+***
+
+####Installation & Setup
+This assumes you already have node.js & npm installed.
+```
+git clone https://github.com/rcpeters/corcid.org node-login
+cd node-login
+npm install -d
+node app
+```
---
+++ app/server/router.js
@@ -0,0 +1,254 @@
+
+var CT = require('./modules/country-list');
+var AM = require('./modules/account-manager');
+var EM = require('./modules/email-dispatcher');
+var http = require("http");
+
+module.exports = function(app) {
+
+// main login page //
+
+	app.get('/', function(req, res){
+	// check if the user's credentials are saved in a cookie //
+		if (req.cookies.user == undefined || req.cookies.pass == undefined){
+			res.render('login', { title: 'Hello - Please Login To Your Account' });
+		}	else{
+	// attempt automatic login //
+			AM.autoLogin(req.cookies.user, req.cookies.pass, function(o){
+				if (o != null){
+				    req.session.user = o;
+					res.redirect('/home');
+				}	else{
+					res.render('login', { title: 'Hello - Please Login To Your Account' });
+				}
+			});
+		}
+	});
+	
+	app.post('/', function(req, res){
+		AM.manualLogin(req.param('user'), req.param('pass'), function(e, o){
+			if (!o){
+				res.send(e, 400);
+			}	else{
+			    req.session.user = o;
+				if (req.param('remember-me') == 'true'){
+					res.cookie('user', o.user, { maxAge: 900000 });
+					res.cookie('pass', o.pass, { maxAge: 900000 });
+				}
+				res.send(o, 200);
+			}
+		});
+	});
+	
+// logged-in user homepage //
+	
+	app.get('/home', function(req, res) {
+	    if (req.session.user == null){
+	// if user is not logged-in redirect back to login page //
+	        res.redirect('/');
+	    }   else{
+			res.render('home', {
+				title : 'Control Panel',
+				countries : CT,
+				udata : req.session.user
+			});
+	    }
+	});
+	
+	app.post('/home', function(req, res){
+		if (req.param('user') != undefined) {
+			AM.updateAccount({
+				user 		: req.param('user'),
+				name 		: req.param('name'),
+				email 		: req.param('email'),
+				country 	: req.param('country'),
+				pass		: req.param('pass'),
+				orcid		: req.param('orcid'),
+				url			: req.param('url')
+			}, function(e, o){
+				if (e){
+					res.send('error-updating-account', 400);
+				}	else{
+					req.session.user = o;
+			// update the user's login cookies if they exists //
+					if (req.cookies.user != undefined && req.cookies.pass != undefined){
+						res.cookie('user', o.user, { maxAge: 900000 });
+						res.cookie('pass', o.pass, { maxAge: 900000 });	
+					}
+					res.send('ok', 200);
+				}
+			});
+		}	else if (req.param('logout') == 'true'){
+			res.clearCookie('user');
+			res.clearCookie('pass');
+			req.session.destroy(function(e){ res.send('ok', 200); });
+		}
+	});
+	
+// creating new accounts //
+	
+	app.get('/signup', function(req, res) {
+		res.render('signup', {  title: 'Signup', countries : CT });
+	});
+
+	app.get('/login', function(req, res) {
+		res.render('login', {  title: 'Signup', countries : CT });
+	});
+
+	
+	app.post('/signup', function(req, res){
+		AM.addNewAccount({
+			name 	: req.param('name'),
+			email 	: req.param('email'),
+			user 	: req.param('user'),
+			pass	: req.param('pass'),
+			country : req.param('country'),
+			url : req.param('url'),
+			orcid : req.param('orcid')
+		}, function(e){
+			if (e){
+				res.send(e, 400);
+			}	else{
+				res.send('ok', 200);
+			}
+		});
+	});
+
+// password reset //
+
+	app.post('/lost-password', function(req, res){
+	// look up the user's account via their email //
+		AM.getAccountByEmail(req.param('email'), function(o){
+			if (o){
+				res.send('ok', 200);
+				EM.dispatchResetPasswordLink(o, function(e, m){
+				// this callback takes a moment to return //
+				// should add an ajax loader to give user feedback //
+					if (!e) {
+					//	res.send('ok', 200);
+					}	else{
+						res.send('email-server-error', 400);
+						for (k in e) console.log('error : ', k, e[k]);
+					}
+				});
+			}	else{
+				res.send('email-not-found', 400);
+			}
+		});
+	});
+
+	app.get('/reset-password', function(req, res) {
+		var email = req.query["e"];
+		var passH = req.query["p"];
+		AM.validateResetLink(email, passH, function(e){
+			if (e != 'ok'){
+				res.redirect('/');
+			} else{
+	// save the user's email in a session instead of sending to the client //
+				req.session.reset = { email:email, passHash:passH };
+				res.render('reset', { title : 'Reset Password' });
+			}
+		})
+	});
+	
+	app.post('/reset-password', function(req, res) {
+		var nPass = req.param('pass');
+	// retrieve the user's email from the session to lookup their account and reset password //
+		var email = req.session.reset.email;
+	// destory the session immediately after retrieving the stored email //
+		req.session.destroy();
+		AM.updatePassword(email, nPass, function(e, o){
+			if (o){
+				res.send('ok', 200);
+			}	else{
+				res.send('unable to update password', 400);
+			}
+		})
+	});
+	
+// view & delete accounts //
+	
+	app.get('/print', function(req, res) {
+		AM.getAllRecords( function(e, accounts){
+			res.render('print', { title : 'Account List', accts : accounts });
+		})
+	});
+	
+	app.post('/delete', function(req, res){
+		AM.deleteAccount(req.body.id, function(e, obj){
+			if (!e){
+				res.clearCookie('user');
+				res.clearCookie('pass');
+	            req.session.destroy(function(e){ res.send('ok', 200); });
+			}	else{
+				res.send('record not found', 400);
+			}
+	    });
+	});
+	
+	app.get('/reset', function(req, res) {
+		AM.delAllRecords(function(){
+			res.redirect('/print');	
+		});
+	});
+	
+	app.get('*', function(req, res) { 
+		var path = req.url;
+		console.log(path);
+		if (path.indexOf(".") !== -1) {
+			console.log("end response");
+			res.render('404', { title: 'Page Not Found'});
+			return false;
+		}
+		
+		var orcOutput = null;
+		AM.getAccountByUrl(path, function(o){
+			console.log("object is " + o);
+			
+			if (o != null) {
+				
+				
+				orcReq = http.get({
+						host: 'orcid.org',
+  						port: 80,
+  						path:  "/" + o.orcid
+				}, function(orcRes) {
+				  console.log("Got response: " + res.statusCode);
+				  
+					orcRes.on('data', function (chunk) {
+    	        		orcOutput += chunk;
+    	        		console.log("chunck" + chunk);
+        			});
+			
+					orcRes.on('error', function(e) {
+  					  console.log("Got error: " + e.message);
+					});
+				
+					orcRes.on('end', function() {
+						if (orcOutput == null) 
+						res.render('404', { title: 'Page Not Found'});
+						else {
+							 orcOutput = orcOutput.replace(/<header(.|[\r\n])*<\/header>/,"");
+							 orcOutput = orcOutput.replace(/<footer(.|[\r\n])*<\/footer>/,"");
+							 orcOutput = orcOutput.replace(/[^'"']*bootstrap.css[^'"']*/,"/vendor/geo-bootstrap.css");
+							 orcOutput = orcOutput.replace(/[^'"']*orcid.css[^'"']*/,"");
+						     res.send(orcOutput);
+						} 
+	
+					});
+			
+				
+ 	       		});
+ 	       		
+				
+				orcReq.end();
+				
+				
+			}
+		});
+		
+		
+		
+	});
+
+};
